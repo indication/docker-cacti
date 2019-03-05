a281d2fee9faabb2b9eb30a4f7516a26a44a09f4
in_dow <in_dow@hotmail.com>
2019-03-06 01:34:35 +0900

    Fix deployment errors

--- cacti/entrypoint.sh
+++ cacti/entrypoint.sh
@@ -5,14 +5,29 @@
 : "${DB_DATABASE:?DB_DATABASE needs to be set}"
 : "${DB_USERNAME:?DB_USERNAME needs to be set}"
 : "${DB_PASSWORD:?DB_PASSWORD needs to be set}"
+: "${TZ:?TZ needs to be set}"
 MYSQLCMDBASE="mysql --host=${DB_HOST} --port=${DB_PORT:-3306} -ns --user=${DB_USERNAME} --password=${DB_PASSWORD} --database=${DB_DATABASE}"
-$MYSQLCMDBASE -w --connect-timeout=100 -e "SELECT 'OK';" ||  echo "Failed to access ${DB_HOST}:${DB_PORT:-3306}" || exit 1;
-# setup for glpi
+
+echo "date.timezone = '${TZ}'" > /usr/local/etc/php/conf.d/zzz-timezone.ini
+
+for i in `seq 1 10`
+do
+  $MYSQLCMDBASE -w --connect-timeout=1000 -e "SELECT 'OK';"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  if [ $i -eq 10 ]; then
+    echo "Failed to access ${DB_HOST}:${DB_PORT:-3306}"
+    exit 1
+  fi
+  sleep 1
+done
+# setup for cacti
 CONFFILE=/var/www/html/cacti/include/config.php.done
 $MYSQLCMDBASE -w --connect-timeout=100 -e "SELECT * FROM version WHERE 1=0;"
 if [ $? -gt 0 ]; then
   echo Setup data
-  $MYSQLCMDBASE < cacti.sql
+  $MYSQLCMDBASE < /var/www/html/cacti/cacti.sql
 fi
 
 
--- mysql.init/20_cacti.sql
+++ mysql.init/20_cacti.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE cacti;
 create user 'cacti'@'%' identified by 'cactipass';
 grant all on cacti.* to 'cacti'@'%';
+grant select on mysql.time_zone_name to 'cacti'@'%';
 FLUSH PRIVILEGES;
-
