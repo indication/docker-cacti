c8d473a4fb6862aceed7efc52943e847af100d3a
in_dow <in_dow@hotmail.com>
2019-03-06 01:47:01 +0900

    Fix mysql params

--- mysql.init/20_cacti.sql
+++ mysql.init/20_cacti.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE cacti;
+CREATE DATABASE cacti default character set utf8mb4 ;
 create user 'cacti'@'%' identified by 'cactipass';
 grant all on cacti.* to 'cacti'@'%';
 grant select on mysql.time_zone_name to 'cacti'@'%';
