1e21790f63e6c3d4aa91ed461e2f3c67a3b454b4
in_dow <in_dow@hotmail.com>
2019-03-08 00:13:30 +0900

    Fix timeouts

--- cacti/entrypoint.sh
+++ cacti/entrypoint.sh
@@ -9,8 +9,9 @@
 MYSQLCMDBASE="mysql --host=${DB_HOST} --port=${DB_PORT:-3306} -ns --user=${DB_USERNAME} --password=${DB_PASSWORD} --database=${DB_DATABASE}"
 
 echo "date.timezone = '${TZ}'" > /usr/local/etc/php/conf.d/zzz-timezone.ini
+chown www-data: -R /var/www/html/cacti
 
-for i in `seq 1 10`
+for i in `seq 1 20`
 do
   $MYSQLCMDBASE -w --connect-timeout=1000 -e "SELECT 'OK';"
   if [ $? -eq 0 ]; then
@@ -24,7 +25,7 @@ do
 done
 # setup for cacti
 CONFFILE=/var/www/html/cacti/include/config.php.done
-$MYSQLCMDBASE -w --connect-timeout=100 -e "SELECT * FROM version WHERE 1=0;"
+$MYSQLCMDBASE -w --connect-timeout=1000 -e "SELECT * FROM version WHERE 1=0;"
 if [ $? -gt 0 ]; then
   echo Setup data
   $MYSQLCMDBASE < /var/www/html/cacti/cacti.sql
